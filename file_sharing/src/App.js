55baec4239794dc854b7b05d54ab382cc7a2efbe
tiwarimanvi <manvitiwari029@gmail.com>
2023-11-04 22:42:09 +0530

    wallet page

--- file_sharing/src/App.js
+++ file_sharing/src/App.js
@@ -1,14 +1,18 @@
 
-import { dividerClasses } from '@mui/material';
 import './App.css';
+import Button from './components/Button'
+import Heading from './components/Heading';
 
 function App() {
   return (
-    <div className='text-xl text-black'>
-      <p >File Sharing </p>
+    <div className='flex flex-col items-center justify-center h-screen bg-black border-dotted border-2 border-color: rgb(229 231 235) '>
+      <div className=' pb-28'>
+      <Heading/>
+      </div>
+    <div className=' pb-40'>
+      <Button/>
     </div>
-    
-    
+    </div>  
   );
 }
 
