a55b093d7e12c74393260abbbd71d0f2874e777c
tiwarimanvi <manvitiwari029@gmail.com>
2023-11-05 02:00:22 +0530

    corrected ui for wallet connect page

--- file_sharing/src/App.js
+++ file_sharing/src/App.js
@@ -1,21 +1,17 @@
 
 import Button from './components/Button'
 import Heading from './components/Heading';
+import Loader from './components/Loader';
 import './App.css';
-import ConnectWallet from './components/connectWallet';
-import Drive from './contracts/Drive.sol/Drive.json'
-import { ethers } from "ethers";
-import { useState, useEffect } from "react";
-import Loader from './components/loader.js';
 
 function App() {
   return (
-    
-    <div className='flex flex-col items-center justify-center h-screen bg-black border-dotted border-2 border-color: rgb(229 231 235)'>
-      <div className=' pb-28'>
+    <div className='flex flex-col items-center justify-center h-screen bg-black '>
+      {/* <Loader/> */}
+      <div className=' pt-28 pb-20'>
       <Heading/>
       </div>
-    <div className=' pb-40'>
+    <div className='pt-8'>
       <Button/>
     </div>
     </div>    
