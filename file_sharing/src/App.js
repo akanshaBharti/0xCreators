fc7535167d1a61c5e5ca681470f2ded7109bdb1d
Jyotendra02 <jyotendrachandan02@gmail.com>
2023-11-04 23:37:26 +0530

    created wallet component

--- file_sharing/src/App.js
+++ file_sharing/src/App.js
@@ -1,18 +1,10 @@
 
 import './App.css';
-import Button from './components/Button'
-import Heading from './components/Heading';
+import Wallet from './components/Wallet';
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-black border-dotted border-2 border-color: rgb(229 231 235) '>
-      <div className=' pb-28'>
-      <Heading/>
-      </div>
-    <div className=' pb-40'>
-      <Button/>
-    </div>
-    </div>  
+    <Wallet/>
   );
 }
 
---
+++ file_sharing/src/components/Wallet.js
@@ -0,0 +1,17 @@
+import Button from './Button'
+import Heading from './Heading';
+
+function Wallet() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-black border-dotted border-2 border-color: rgb(229 231 235) '>
+      <div className=' pb-28'>
+      <Heading/>
+      </div>
+    <div className=' pb-40'>
+      <Button/>
+    </div>
+    </div>  
+  );
+}
+
+export default Wallet;
