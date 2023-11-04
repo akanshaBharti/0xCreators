63d6f894a4f7e555bc979b0dc5e39566cd78e51f
Shreyas <shreyassingh621@gmail.com>
2023-11-05 02:12:40 +0530

    File Upload Feature created

--- file_sharing/src/App.js
+++ file_sharing/src/App.js
@@ -4,22 +4,17 @@ import Heading from './components/Heading';
 import './App.css';
 import ConnectWallet from './components/connectWallet';
 import Drive from './contracts/Drive.sol/Drive.json'
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 import Loader from './components/Loader.js';
-import AddFile from './components/addFile.js';
+import FileUpload from './components/addFile.tsx';
+import Wallet from "./components/Wallet.js"
 
 function App() {
   return (
-    
-    <div className='flex flex-col items-center justify-center h-screen bg-black border-dotted border-2 border-color: rgb(229 231 235)'>
-      <div className=' pb-28'>
-      <Heading/>
-      </div>
-    <div className=' pb-40'>
-      <Button/>
-    </div>
-    <AddFile/>
+    <div>
+
+    <FileUpload/>
     </div>    
   );
 }
