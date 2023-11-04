5047f6f5864f60e90272a4513fb848bbf53584ec
Jyotendra02 <jyotendrachandan02@gmail.com>
2023-11-04 16:08:17 +0530

    Create React App and Tailwind

---
+++ file_sharing/src/App.js
@@ -0,0 +1,15 @@
+
+import { dividerClasses } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-xl text-black'>
+      <p >File Sharing </p>
+    </div>
+    
+    
+  );
+}
+
+export default App;
