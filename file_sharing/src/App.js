413bdc02e77140fe81596a6b9c5e32d360d0ec57
Akansha <akanshabharti1213@gmail.com>
2023-11-05 02:13:49 +0530

    added ether to app.js

--- file_sharing/src/App.js
+++ file_sharing/src/App.js
@@ -1,4 +1,3 @@
-
 import Button from './components/Button'
 import Heading from './components/Heading';
 import './App.css';
@@ -9,7 +8,45 @@ import { useState, useEffect } from "react";
 import Loader from './components/Loader.js';
 import AddFile from './components/addFile.js';
 
+
 function App() {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+
+  useEffect(() => { 
+    const provider = new ethers.BrowserProvider(window.ethereum)
+
+    const loadProvider = async () => {
+      if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        }); 
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = "0x6481321f1fC160321eAC3A47104CE595301CF3e3";
+ 
+        const contract = new ethers.Contract(
+          contractAddress,
+          Drive.abi,
+          signer
+        );
+        console.log(contract);
+        setContract(contract);
+        setProvider(provider);
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+    provider && loadProvider();
+  }, []);
+
   return (
     
     <div className='flex flex-col items-center justify-center h-screen bg-black border-dotted border-2 border-color: rgb(229 231 235)'>
