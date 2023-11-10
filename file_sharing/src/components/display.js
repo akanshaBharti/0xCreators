dfbc6337716edc231c7152e73442bef1c993d88a
Akansha <akanshabharti1213@gmail.com>
2023-11-10 16:52:49 +0530

    Get api not working

--- file_sharing/src/components/display.js
+++ file_sharing/src/components/display.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./Css/display.css";
+
 const Display = ({ contract, account }) => {
   const [data, setData] = useState("");
+
   const getdata = async () => {
     let dataArray;
     const Otheraddress = document.querySelector(".address").value;
     try {
-      if (Otheraddress) { 
+      if (Otheraddress) {
         dataArray = await contract.display(Otheraddress);
-        console.log("dataArray",dataArray); 
+        console.log("dataArray", dataArray);
       } else {
         dataArray = await contract.display(account);
       }
@@ -18,7 +20,7 @@ const Display = ({ contract, account }) => {
     const isEmpty = Object.keys(dataArray).length === 0;
 
     if (!isEmpty) {
-      const str = dataArray.toString(); 
+      const str = dataArray.toString();
       const str_array = str.split(",");
       const images = str_array.map((item, i) => {
         return (
